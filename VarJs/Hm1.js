2cba22da80a05a0b73a2feab375a0e2e47f95e4e
Efr1tores <olegbobrov740@gmail.com>
2023-09-14 20:23:50 +0300

    scr

---
+++ VarJs/Hm1.js
@@ -0,0 +1,45 @@
+//Змінна
+let a;
+console.log(a);
+a="Password";
+console.log(a);
+a=1;
+console.log(a);
+var b;
+b=0
+console.log(b)
+const g="S1d";
+console.log(g);
+
+
+//typeof
+let r;
+r='Земля';
+console.log(typeof(r));
+
+let f;
+f=24;
+
+let d;
+d=24;
+
+let s;
+s=f+d;
+console.log(s);
+
+let n;
+n=null;
+console.log(n);
+
+let v;
+console.log(v);
+
+let bool;
+bool=5+6>9;
+console.log(bool);
+
+let bigint;
+bigint=234582382838458238958293312354421512521543255645675n;
+console.log(bigint)
+
+    
--- VarJs/Hm2.js
+++ VarJs/Hm2.js
@@ -1,5 +1,5 @@
 console.log(+'0'+'10')//010
-console.log(-5+10)//-5
-console.log('true '+'false')/1
+console.log('-5++10')//-5
+console.log('true '+'false')//1
 console.log('10'/'5')
 console.log('6'*'3')
