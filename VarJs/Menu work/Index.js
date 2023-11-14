6af4c02ef9def422b9f25c0a304b509656439e85
Efr1tores <olegbobrov740@gmail.com>
2023-11-14 19:03:42 +0200

    New Cod

--- VarJs/Menu work/Index.js
+++ VarJs/Menu work/Index.js
@@ -1,21 +1,27 @@
 "use strict"
-const searchIcon=document.getElementById('search-icon');
-const searchInput=document.getElementById('search-input');
+const searchIcon = document.getElementById('search-icon');
+const searchInput = document.getElementById('search-input');
 
-document.documentElement.addEventListener('click',(e)=>{
- let elem=e.target;
- if(elem===searchIcon||elem===searchInput){searchIcon.style.display='none'; 
- searchInput.style.display='block';
-searchInput.focus();}
- 
-else{searchIcon.style.display='block';
-searchInput.style.display='none';
-}});
+document.documentElement.addEventListener('click', (e) => {
+    let elem = e.target;
+    if (elem === searchIcon || elem === searchInput) {
+        searchIcon.style.display = 'none';
+        searchInput.style.display = 'block';
+        searchInput.focus();
+    }
 
+    else {
+        searchIcon.style.display = 'block';
+        searchInput.style.display = 'none';
+    }
+});
 
+const statement = document.getElementById('statement');
+const modalForm = document.getElementById('model-form');
+const closeModelForm = document.getElementById('close-model-form');
+statement.addEventListener('click', (e) => { e.preventDefault(); modalForm.classList.remove('d-none'); });
 
-
-
+closeModelForm.addEventListener('click', (e) => { e.preventDefault(); modalForm.classList.add('d-none'); });
 
 
 
