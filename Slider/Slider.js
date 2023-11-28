44e5430ae8ae566b156b925460a23d2f1e493d05
Efr1tores <olegbobrov740@gmail.com>
2023-11-28 19:51:03 +0200

    sdr

--- Slider/Slider.js
+++ Slider/Slider.js
@@ -3,6 +3,10 @@ const Slider = document.getElementById('slides');
 
 const slides = Slider.querySelectorAll('.slider__slide');
 
+const arrowPrev = document.getElementById('arrow-prev');
+
+const arrowNext = document.getElementById('arrow-next');
+
 let currentSlide = 0;
 
 
@@ -11,13 +15,17 @@ let slideInterval;
 const slideTime = 2000;
 function continueSlideInterval() {
     slideInterval = setInterval(nextSlied, slideTime);
-    
+
 }
 function stopSlide() { clearInterval(slideInterval); };
 
 function slideReset() { slides[currentSlide].classList.remove('.slider__slide_showing'); };
 
 function slideSet() { slides[currentSlide].classList.add('slider__slide_showing'); };
+
+function showNextSlide() { stopSlide(); nextSlied(); }
+
+function showPrevSlides() { stopSlide(); slideReset(); slideSet(); }
 
 function nextSlied() {
     slideReset();
@@ -31,3 +39,5 @@ continueSlideInterval();
 Slider.addEventListener('mouseover', stopSlide);
 Slider.addEventListener('mouseover', continueSlideInterval);
 
+arrowNext.addEventListener('click', showNextSlide);
+arrowPrev.addEventListener('click',showPrevSlides);
