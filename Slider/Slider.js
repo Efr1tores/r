fa0409640a3cdd94143e1a8dfb32ed08df54ac7b
Efr1tores <olegbobrov740@gmail.com>
2024-01-02 19:14:26 +0200

    Error

--- Slider/Slider.js
+++ Slider/Slider.js
@@ -7,37 +7,53 @@ const arrowPrev = document.getElementById('arrow-prev');
 
 const arrowNext = document.getElementById('arrow-next');
 
+const toggleRadio = document.getElementById('toggle_radio');
+
+const toggleInput = toggleRadio.querySelectorAll('input');
+
 let currentSlide = 0;
 
 
 let slideInterval;
 
-const slideTime = 2000;
-function continueSlideInterval() {
-    slideInterval = setInterval(nextSlied, slideTime);
-
-}
-function stopSlide() { clearInterval(slideInterval); };
-
-function slideReset() { slides[currentSlide].classList.remove('.slider__slide_showing'); };
-
-function slideSet() { slides[currentSlide].classList.add('slider__slide_showing'); };
-
-function showNextSlide() { stopSlide(); nextSlied(); }
-
-function showPrevSlides() { stopSlide(); slideReset(); slideSet(); }
 
 function nextSlied() {
+    
     slideReset();
-    currentSlide = (currentSlide + 1) % slides.length;
+   currentSlide= (currentSlide+1)%slides.length;
     slideSet();
 
 };
 
+const slideTime = 2000;
+
+function continueSlideInterval() {
+    slideInterval = setInterval(nextSlied, slideTime);
+
+}
+function stopSlide() { clearInterval(slideInterval); }
+
+function slideReset() { slides[currentSlide].classList.remove('slider__slide_showing'); }
+
+function slideSet() { slides[currentSlide].classList.add('slider__slide_showing');
+ toggleInput[currentSlide].checked=true};
+
+function showNextSlide() { stopSlide(); nextSlied(); }
+
+function showPrevSlides() { stopSlide(); slideReset(); 
+    currentSlide= (currentSlide==0)?slides.length-1:currentSlide-1;
+    slideSet(); }
+
 
 continueSlideInterval();
 Slider.addEventListener('mouseover', stopSlide);
-Slider.addEventListener('mouseover', continueSlideInterval);
+Slider.addEventListener('mouseout', continueSlideInterval);
 
 arrowNext.addEventListener('click', showNextSlide);
-arrowPrev.addEventListener('click',showPrevSlides);
+arrowPrev.addEventListener('click', showPrevSlides);
+
+function toggleSlide(event){stopSlide(); slideReset(); currentSlide=event.target.value;
+slideSet();
+
+}
+toggleRadio.addEventListener('input',toggleSlide);
